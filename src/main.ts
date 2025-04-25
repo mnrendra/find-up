56b292331a2eb68418d1d0eace0aead52f519f00
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-04-26 04:59:42 +0700

    docs: update readme (#8)
    issues:
    * docs: update readme (#7)
    
    commits:
    * docs: update readme (b17078b)

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,19 @@ import { findUp } from './utils'
 /**
  * Finds a file or directory by searching upward from a specific directory.
  *
- * If no initial directory is provided, the search will start from the current
- * working directory.
+ * If no initial directory is provided, it will start from the current working
+ * directory (`process.cwd()`).
  *
- * @param {string} target - The name of the file or directory to find.
- * @param {string} [initDir] - Optional directory to start searching from
- * (defaults to `process.cwd()`).
+ * @param {string} target - The name of the file or directory to
+ * find.
+ *
+ * @param {string} [initDir] - Optional directory to start searching from.
+ * Defaults to `process.cwd()`.
  *
  * @returns {string|null} The absolute path of the file or directory if found,
  * or `null` if not found.
  *
- * @see https://github.com/mnrendra/find-up#readme
+ * @see {@link https://github.com/mnrendra/find-up#findup documentation}
  */
 const main = (
   target: string,
